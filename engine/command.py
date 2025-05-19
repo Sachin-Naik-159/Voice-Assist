083903d1890465a604a523461fc39840b023385b
Sachin-Naik-159 <sacqwerty123@gmail.com>
2025-05-19 19:05:05 +0530

    text 2 speeh

--- engine/command.py
+++ engine/command.py
@@ -1,13 +1,36 @@
 import pyttsx3
-
+import speech_recognition as sr
+import eel
+import time
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
-    voices = engine.getProperty('voices')
+    voices = engine.getProperty('voices') 
     engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 155)
-    # print(voices)
+    engine.setProperty('rate', 174)
     engine.say(text)
     engine.runAndWait()
 
-speak("Testing Testing")
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+text = takecommand()
+
+speak(text)
